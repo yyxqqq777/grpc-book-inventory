463e24f51b955b9e411298014715d4d185cca74a
yyxqqq777 <yunxuan517@gmail.com>
2022-12-14 15:26:43 -0500

    fix(client): uncomment some codes

--- client/get_book_titles.py
+++ client/get_book_titles.py
@@ -1,19 +1,18 @@
 from inventory_client import InventoryClient
 
 def get_book_titles(client, isbns):
-    titles = []
+    res = []
     for isbn in isbns:
         book = client.get_book(isbn)
-        titles.append(book.title)
-    return titles
+        res.append(book.title)
+    return res
 
 
 if __name__ == "__main__":
-    # Create an instance of the gRPC client
+    # Create an instance of InventoryClient
     client = InventoryClient("localhost", 8082)
 
-    # Call the get_book_titles function with two hardcoded ISBNs
+    # Call the function with hardcoded ISBNs
     titles = get_book_titles(client, ["978-0984782857", "978-0439708180"])
 
-    # Print the returned titles
     print(titles)
--- service/inventory_server.py
+++ service/inventory_server.py
@@ -9,6 +9,7 @@ import inventory_pb2_grpc
 
 class InventoryService(inventory_pb2_grpc.InventoryServiceServicer):
     def __init__(self):
+        # use a list to store the book data
         book1 = book_pb2.Book(title="Cracking the Coding Interview", isbn="978-0984782857", genre=book_pb2.Genre.FAIRY_TALE, author="Gayle Laakmann McDowell", publishing_year=2015)
         book2 = book_pb2.Book(title="Harry Potter and the Sorcerer's Stone", isbn="978-0439708180", genre=book_pb2.Genre.NOVEL, author="J.K. Rowling", publishing_year=1998)
         self.books = [book1, book2]
